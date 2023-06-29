c59be659d0c63af83543ce0f551a07456f435160
Vladimir Gabov <51012754+Vladimir99users@users.noreply.github.com>
2023-06-29 11:00:40 +1000

    Скрипт для конфигурации данных
    Для будущей передачи данных файла, надо создать конфигурационный файлик, где будет хранится текущие представление о всех визуальных составляющих объекта.

---
+++ Assets/Gallery/Scripts/VisualLogic/Configuration.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Configuration : MonoBehaviour
+{
+   [SerializeField] private Image _viewDataImage;
+   [SerializeField] private Image _fillAmountImage;
+   [SerializeField] private TextMeshProUGUI _textLoadData;
+
+   public Image ViewDataImage {get => _viewDataImage; }
+   public Image FillAmountImage {get => _fillAmountImage; }
+   public TextMeshProUGUI TextLoadData {get => _textLoadData; }
+}
